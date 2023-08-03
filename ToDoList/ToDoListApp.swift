a996e5aaa2339a016be415cf8ab6c7f3b5d4625f
piyushXP0067 <piyush@xpindia.in>
2023-08-03 15:18:57 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by XP India  on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
